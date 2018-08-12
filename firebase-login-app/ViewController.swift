3018beb99ac12f9d1c49b1a1f709f08892ad3459
roeyHonig <roeyhonig94@gmail.com>
2018-08-12 22:44:12 +0300

    connected seguway

--- firebase-login-app/ViewController.swift
+++ firebase-login-app/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func showWelcomeScreen(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToWelcomeScreen", sender: sender)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ firebase-login-app/WelcomeScreenViewController.swift
@@ -0,0 +1,39 @@
+//
+//  WelcomeScreenViewController.swift
+//  firebase-login-app
+//
+//  Created by hackeru on 1 Elul 5778.
+//  Copyright © 5778 student.roey.honig. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeScreenViewController: UIViewController {
+
+    @IBAction func returnToLogin(_ sender: UIButton) {
+        performSegue(withIdentifier: "goBackToLogin", sender: sender)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
